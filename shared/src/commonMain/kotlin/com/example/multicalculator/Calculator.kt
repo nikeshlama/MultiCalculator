ef4a857b908de6f146519a0cff73244fac8462cf
Nikesh Lama <nikeshlama76@gmail.com>
2023-10-02 15:56:24 -0400

    The Multicalculator project has been created Calculator class has been created Add, subtract, multiply, divide functions has been created

---
+++ shared/src/commonMain/kotlin/com/example/multicalculator/Calculator.kt
@@ -0,0 +1,19 @@
+package com.example.multicalculator
+
+class Calculator {
+        fun add(left: Int, right: Int): Int {
+            return left + right
+        }
+
+        fun subtract(left: Int, right: Int): Int {
+            return left - right
+        }
+
+        fun divide(left: Int, right: Int): Int {
+            return left / right
+        }
+
+        fun multiply(left: Int, right: Int): Int {
+            return left * right
+        }
+}
