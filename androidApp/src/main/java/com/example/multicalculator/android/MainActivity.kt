b665abf2bbe45046eec28dafe65c9234cc2974d2
Nikesh Lama <nikeshlama76@gmail.com>
2023-10-13 21:02:36 -0400

    CalcView UI Code done

--- androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -3,12 +3,31 @@ package com.example.multicalculator.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.multicalculator.Greeting
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,38 +38,45 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
-                    CalcView("this is the Calculation View space")
-                    CalcRow("this is the Calculation Row space")
-                    CalcDisplay("this is the Calculation Display space")
-                    CalcNumericButton("this is the Calculation Numeric Button space")
-                    CalcOperationButton("this is the Calculation Operation Button space")
-                    CalcEqualsButton("this is the Calculation Equals Button space")
-
+                    CalcView()
                 }
             }
         }
     }
 }
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MyApplicationTheme {
+        CalcView()
+    }
+}
 //creating CalcView Function
 @Composable
-fun CalcView(calculationView: String) {
-    Text(text = "Calculation View: $calculationView")
+fun CalcView(){
+    val displayText = remember {mutableStateOf("0")}
+    Column(modifier = Modifier.background(Color.LightGray) then  Modifier.padding(0.dp)) {
+        Row {
+            CalcDisplay(displayText)
+        }
+        Row {
+            Column {
+                for (i in 7 downTo 1 step 3) CalcRow(display = displayText, startNum = i, numButtons = 3)
+                Row {
+                    CalcNumericButton(number = 0, display =displayText )
+                    CalcEqualsButton(display = displayText)
+                }
+            }
+            Column {
+                CalcOperationButton(operation = "+", display = displayText  )
+                CalcOperationButton(operation = "-", display = displayText  )
+                CalcOperationButton(operation = "*", display = displayText  )
+                CalcOperationButton(operation = "/", display = displayText  )
+            }
+        }
+    }
 }
-
-//Creating CalcRow Function
-@Composable
-fun CalcRow(calculationRow: String) {
-     Text(text = "Calculation Row: $calculationRow")
-}
-
-//Creating CalcDisplay Function
-@Composable
-fun CalcDisplay(calculationDisplay: String) {
-    Text(text = "Calculation Display: $calculationDisplay")
-}
-
-//Creating CalcNumericButton function
 @Composable
 fun CalcNumericButton (calculationNumericButton: String) {
     Text(text = "Calculation Numeric Button: $calculationNumericButton")
@@ -62,7 +88,7 @@ fun CalcOperationButton (calculationOperationButton: String) {
     Text(text = "Calculation Operation Button: $calculationOperationButton")
 }
 
-//Creating CalcEqualsButton function
+
 @Composable
 fun CalcEqualsButton (calculationEqualsButton: String) {
     Text(text ="Calculation Equals Button: $calculationEqualsButton")
@@ -79,4 +105,6 @@ fun DefaultPreview() {
     MyApplicationTheme {
         GreetingView("Hello, Android!")
     }
+
+
 }
